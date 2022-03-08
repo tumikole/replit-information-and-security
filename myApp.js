c7a0d863753ff2b2994d4e9760e01ebfa078abae
tumikole <samuelramollo@gmail.com>
2022-03-08 17:35:31 +0200

    initial commit

--- myApp.js
+++ myApp.js
@@ -1,18 +1,34 @@
+const { application } = require('express');
 const express = require('express');
 const app = express();
+const helmet = require('helmet')
+
+app.use(helmet.hidePoweredBy())
+app.use(helmet.frameguard({action: 'deny'}))
 
 
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
+app.use(helmet.ieNoOpen())
 
+ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.noCache())
+app.use(helmet.contentSecurityPolicy({directives:{ defaultSrc:["'self'"], scriptSrc:["'self'", 'trusted-cdn.com']}}))
 
-
-
-
-
-
-
-
-
-
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
 
 
 
